2b420afb16e62b9a8048dc445d379bf65bd8fbab
mirman-mtaggart <mtaggart@mirman.org>
2015-09-23 09:34:46 -0700

    Add lesson 2

---
+++ 2_Plane-Speaking/app.js
@@ -0,0 +1,18 @@
+var xPlanes = [
+  "X-1",
+  "X-3",
+  "X-15",
+  "X-29",
+  "X-31",
+  "X-37",
+  "X-45"
+];
+
+// Now that we have our array of planes, we need to add elements to our DOM
+// for each one.
+var e = document. //This line is unfinished! Use a method to capture our <ul> element.
+
+for(var i=0; i<xPlanes.length; i++) {
+  var xPlane = xPlanes[i]; //What does "i" refer to?
+  // Do some things to add <li> elements to our empty list.
+}
