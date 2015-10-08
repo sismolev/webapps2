b39dd8c2d2f480f6172a86bc80bb8fa5c6e89fa6
sismolev <sismolev2018@mirmanmustangs.org>
2015-10-08 16:21:49 -0700

    Finish plane speaking

--- 2_Plane-Speaking/app.js
+++ 2_Plane-Speaking/app.js
@@ -1,23 +1,56 @@
-var xPlanes = {
-  "X-1":"First plane to break the speed of sound",
-  "X-3":"Used to test long-duration high-speed flight",
-  "X-15":"Used for hypersonic high-altitude testing, and was the first manned hypersonic plane with the ability of suborbital space-flight",
-  "X-29":"Had forward-swept wings",
-  "X-31":"Used thrust vectoring",
-  "X-37":"Reusable orbital spaceplane",
-  "X-45":"Unmanned combat plane"
-};
+var xPlanes = [
+{
+    "name":"X-1",
+    "description":"First plane to break the speed of sound",
+    "imageURL": "https://en.wikipedia.org/wiki/List_of_X-planes#/media/File:Bell_X-1_46-062_(in_flight).jpg"
+},
+{
+  "name":"X-3",
+ "description":"Used to test long-duration high-speed flight",
+ "imageURL":"https://en.wikipedia.org/wiki/List_of_X-planes#/media/File:Douglas_X-3_NASA_E-17348.jpg"
+},
+{
+  "name":"X-15",
+ "description":"Used for hypersonic high-altitude testing, and was the first manned hypersonic plane with the ability of suborbital space-flight",
+ "imageURL":"https://en.wikipedia.org/wiki/List_of_X-planes#/media/File:X-15_in_flight.jpg"
+},
+{
+  "name":"X-29",
+ "description":"Had forward-swept wings",
+ "imageURL":"https://en.wikipedia.org/wiki/List_of_X-planes#/media/File:Grumman-X29-InFlight.jpg"
+},
+{
+  "name":"X-31",
+ "description":"Used thrust vectoring",
+ "imageURL":"https://en.wikipedia.org/wiki/List_of_X-planes#/media/File:Rockwell-MBB_X-31_landing.JPG"
+},
+{
+  "name":"X-37",
+ "description":"Reusable orbital spaceplane",
+ "imageURL":"https://en.wikipedia.org/wiki/List_of_X-planes#/media/File:Boeing_X-37B_inside_payload_fairing_before_launch.jpg"
+},
+{
+  "name":"X-45",
+ "description":"Unmanned combat plane",
+ "imageURL":"https://en.wikipedia.org/wiki/List_of_X-planes#/media/File:Boeing_X-45A_UCAV.jpg"
+}
+];
 
 // Now that we have our array of planes, we need to add elements to our DOM
 // for each one.
 var e = document.getElementById("planes-list");
 
-for(var i=0; i<xPlanes.length; i++) {
-  var xPlane = xPlanes[i];
-  e.innerHTML += "<li>" +
-  xPlane +
-  "</li>";
-
-  //What does "i" refer to?
-  // Do some things to add <li> elements to our empty list.
+for(var i in xPlanes) {
+  var xPlane = xPlanes[i]
+  e.innerHTML =
+  "<h1>" +
+  xPlane.name +
+  "</h1>" +
+  "<p>" +
+  xPlane.description +
+  "</p>" +
+  "<img src='" +
+  xPlane.imageURL +
+  "' height='30%' width='50%'" +
+  "</img>"
 }
