bc0be8cf21d5ea2cd1b30ee15df491563c575e67
sismolev <sismolev2018@mirmanmustangs.org>
2015-10-08 06:49:17 -0700

    Finish app.js

--- 2_Plane-Speaking/app.js
+++ 2_Plane-Speaking/app.js
@@ -1,28 +1,23 @@
-var xPlanes = [
-  "X-1",
-  "X-3",
-  "X-15",
-  "X-29",
-  "X-31",
-  "X-37",
-  "X-45"
-];
+var xPlanes = {
+  "X-1":"First plane to break the speed of sound",
+  "X-3":"Used to test long-duration high-speed flight",
+  "X-15":"Used for hypersonic high-altitude testing, and was the first manned hypersonic plane with the ability of suborbital space-flight",
+  "X-29":"Had forward-swept wings",
+  "X-31":"Used thrust vectoring",
+  "X-37":"Reusable orbital spaceplane",
+  "X-45":"Unmanned combat plane"
+};
 
 // Now that we have our array of planes, we need to add elements to our DOM
 // for each one.
-var e = document.createElement("ul") //This line is unfinished! Use a method to capture our <ul> element.
+var e = document.getElementById("planes-list");
 
 for(var i=0; i<xPlanes.length; i++) {
   var xPlane = xPlanes[i];
-  <ol id="planes-list">
-  <li>X-1</li>
-  <li>X-3</li>
-  <li>X-15</li>
-  <li>X-29</li>
-  <li>X-31</li>
-  <li>X-37</li>
-  <li>X-45</li>
-  </ol>
+  e.innerHTML += "<li>" +
+  xPlane +
+  "</li>";
+
   //What does "i" refer to?
   // Do some things to add <li> elements to our empty list.
 }
