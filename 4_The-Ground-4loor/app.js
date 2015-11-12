855acb78951c2dc41c9204a3222f494102a8fd62
sismolev <sismolev2018@mirmanmustangs.org>
2015-11-12 15:14:13 -0800

    The Ground 4loor

--- 4_The-Ground-4loor/app.js
+++ 4_The-Ground-4loor/app.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
   $("#contact-form").submit(function(event){
     event.preventDefault();
+
+    var Data = {
+      firstName: $("#firstname").val(),
+      lastName: $("#lastname").val(),
+      email: $("#email").val(),
+      message: $("#message").val()
+    }
+    console.log(Data);
+
     //Use jQuery to collect the values from the fields and create a
     //JSON object containing them. Then, log the result to the console
 
